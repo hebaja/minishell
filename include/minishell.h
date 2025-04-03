688a891d4e3e38c0ffb6caf9d2a9e494456eb10a
Henrique <hebaja@yahoo.com.br>
2025-04-03 20:04:30 -0300

    feat: 🎸 Parser - Lexical Analysis
    Minishell header file built

---
+++ include/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hebatist <hebatist@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 19:56:15 by hebatist          #+#    #+#             */
+/*   Updated: 2025/04/03 19:56:19 by hebatist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef enum e_token_type
+{
+	WORD,
+	PIPE,
+	APPEND,
+	HEREDOC,
+	REDIRECT_IN,
+	REDIRECT_OUT
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
