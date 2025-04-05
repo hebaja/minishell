ade3cf48898c5194bc8cbb7024e2ea1b11abf99d
Henrique <hebaja@yahoo.com.br>
2025-04-04 23:32:47 -0300

    ci: 🎡 Libft update
    Libft was updated for inclusion of ft_isspace and enhancing header files

--- libft/include/ft_printf.h
+++ libft/include/ft_printf.h
@@ -20,7 +20,6 @@
 int		ft_putchar_len(char c);
 int		ft_putstr_len(char *str);
 int		print_type(const char *str, va_list args);
-int		ft_printf(const char *str, ...);
 void	ft_putnbr_base_len(long nbr, char *base, int *len);
 void	ft_putptr_base_len(unsigned long nbr, char *base, int *len);
 
