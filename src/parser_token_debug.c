1b434e76bf01115ae225f50d7fffa56c807ca712
hebatist <hebaja@yahoo.com.br>
2025-04-08 00:12:38 -0300

    feat: 🎸 Parser - Lexical Analysis
    Adding the last token types

--- src/parser_token_debug.c
+++ src/parser_token_debug.c
@@ -28,6 +28,14 @@ char	*print_token_type(t_token_type type)
 		return ("REDIRECT_IN");
 	if (type == REDIRECT_OUT)
 		return ("REDIRECT_OUT");
+	if (type == BACKGROUND)
+		return ("BACKGROUND");
+	if (type == WILDCARD)
+		return ("WILDCARD");
+	if (type == AND)
+		return ("AND");
+	if (type == OR)
+		return ("OR");
 	return ("");
 }
 
