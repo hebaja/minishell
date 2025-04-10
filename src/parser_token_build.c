40253e5107424b21f3cfff857c3591b26fa3bf5b
hebatist <hebaja@yahoo.com.br>
2025-04-09 23:46:17 -0300

    feat: 🎸 Parser - Syntax Analysis
    Parser dealing with metacharacters as in bash

--- src/parser_token_build.c
+++ src/parser_token_build.c
@@ -42,10 +42,8 @@ t_token_type	define_type(char *value)
 
 	if (ft_strncmp(value, "-", 1) == 0)
 	{
-		value++;
-		if (ft_strncmp(value, " ", 1) != 0)
+		if (ft_strncmp(value + 1, " ", 1) != 0)
 			return (FLAG);
-		value--;
 	}
 	else if (ft_strncmp(value, "|", 2) == 0)
 		return (PIPE);
@@ -112,6 +110,15 @@ int	append_token(t_token **tokens_head, char *value_start, size_t size)
 	return (1);
 }
 
+int	is_metacharacter(char *value)
+{
+	if ((ft_strncmp(value, ">>", 2) == 0) || (ft_strncmp(value, "<<", 2) == 0) || (ft_strncmp(value, "&&", 2) == 0) || (ft_strncmp(value, "||", 2) == 0))
+ 		return (2);
+	if ((ft_strncmp(value, "|", 1) == 0) || (ft_strncmp(value, ">", 1) == 0) || (ft_strncmp(value, "<", 1) == 0) || (ft_strncmp(value, "&", 1) == 0))
+		return (1);
+	return (0);
+}
+
 int	token_lst_build(t_token **tokens_head, char *value)
 {
 	size_t		size;
@@ -121,15 +128,25 @@ int	token_lst_build(t_token **tokens_head, char *value)
 	size = 0;
 	while (*value)
 	{
-		while (*value && !ft_isspace(*value))
+		while (*value && (!ft_isspace(*value) && !is_metacharacter(value)))
 		{
 			value++;
 			size++;
 		}
-		if (!append_token(tokens_head, value_start, size))
-			return (0);
-		while (*value && ft_isspace(*value))
-			value++;
+		if (size)
+			if (!append_token(tokens_head, value_start, size))
+				return (0);
+		if (*value && is_metacharacter(value))
+		{
+			size = is_metacharacter(value);
+			value_start = value;
+			if (!append_token(tokens_head, value_start, size))
+				return (0);
+			value = value + size;
+		}
+		if (ft_isspace(*value))
+			while (*value && ft_isspace(*value))
+				value++;
 		if (*value)
 			value_start = value;
 		size = 0;
