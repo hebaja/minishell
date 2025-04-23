3139c81cfde1e253c6cfad87ce5da71348b7331a
Henrique <hebaja@yahoo.com.br>
2025-04-23 01:52:40 -0300

    refactor: 💡 Parser - Lexical Analysis
    Refactoring for a better coding structure

--- testing/minishell_test_builtin.c
+++ testing/minishell_test_builtin.c
@@ -1,221 +1,109 @@
 #include "minishell_test.h"
 
-Test(minishell_test_suite, build_token_lst_test_all_builtins)
+Test(minishell_test_suite_builtin, build_token_lst_test_all_builtins)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "echo cd pwd export unset env exit";
-	char		**values = ft_split(input, ' ');
-	char		**types;
 
-	token_lst = NULL;
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
  
-Test(minishell_test_suite, build_token_lst_test_builtin_echo)
+Test(minishell_test_suite_builtin, build_token_lst_test_builtin_echo)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "echo -n Hello";
-	char		**values = ft_split(input, ' ');
-	char		**types;
-
-	token_lst = NULL;
+	
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
 
-Test(minishell_test_suite, build_token_lst_test_builting_cd)
+Test(minishell_test_suite_builtin, build_token_lst_test_builting_cd)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "cd /home/user/folder";
-	char		**values = ft_split(input, ' ');
-	char		**types;
-
-	token_lst = NULL;
+	
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
 
-Test(minishell_test_suite, build_token_lst_builtin_pwd)
+Test(minishell_test_suite_builtin, build_token_lst_builtin_pwd)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "pwd";
-	char		**values = ft_split(input, ' ');
-	char		**types;
-
-	token_lst = NULL;
+	
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
 
-Test(minishell_test_suite, build_token_lst_builtin_export)
+Test(minishell_test_suite_builtin, build_token_lst_builtin_export)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "export VARIABLE=VALUE";
-	char		**values = ft_split(input, ' ');
-	char		**types;
 
-	token_lst = NULL;
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
 
-Test(minishell_test_suite, build_token_lst_builtin_unset)
+Test(minishell_test_suite_builtin, build_token_lst_builtin_unset)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "unset VARIABLE";
-	char		**values = ft_split(input, ' ');
-	char		**types;
-
-	token_lst = NULL;
+	
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
 
-Test(minishell_test_suite, build_token_lst_builtin_env)
+Test(minishell_test_suite_builtin, build_token_lst_builtin_env)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "env";
-	char		**values = ft_split(input, ' ');
-	char		**types;
-
-	token_lst = NULL;
+	
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
 
-Test(minishell_test_suite, build_token_lst_builtin_exit)
+Test(minishell_test_suite_builtin, build_token_lst_builtin_exit)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "exit";
-	char		**values = ft_split(input, ' ');
-	char		**types;
 
-	token_lst = NULL;
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
 
-Test(minishell_test_suite, build_token_lst_builtin_mixed)
+Test(minishell_test_suite_builtin, build_token_lst_builtin_mixed)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "echo >> doc || cat --abord < ls -l";
-	char		**values = ft_split(input, ' ');
-	char		**types;
 
-	token_lst = NULL;
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
 
-Test(minishell_test_suite, build_token_lst_builtin_mixed_2)
+Test(minishell_test_suite_builtin, build_token_lst_builtin_mixed_2)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
 	char	*input = "echo>>doc || less<more";
-	char	**values = malloc(sizeof(char *) * 8);
-	char	**types = malloc(sizeof(char *) * 8);
-
+	
+	values = malloc(sizeof(char *) * 8);
 	values[0] = ft_strdup("echo");
 	values[1] = ft_strdup(">>");
 	values[2] = ft_strdup("doc");
@@ -224,16 +112,8 @@ Test(minishell_test_suite, build_token_lst_builtin_mixed_2)
 	values[5] = ft_strdup("<");
 	values[6] = ft_strdup("more");
 	values[7] = NULL;
-	token_lst = NULL;
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	test_lst(token_lst, values, types);
 }
--- testing/minishell_test_extra_meta.c
+++ testing/minishell_test_extra_meta.c
@@ -1,25 +1,95 @@
 #include "minishell_test.h"
 
-Test(minishell_test_suite, build_token_lst_test_dolar_var)
+void	setup_alloc_mem(void)
 {
-	int		i;
-	int		res;
-	t_token	*token_lst;
-	char	*input = "echo $USER";
-	char		**values = ft_split(input, ' ');
-	char		**types;
+	values = malloc(sizeof(char *) * 4);
+	types = malloc(sizeof(char *) * 4);
+}
 
-	token_lst = NULL;
+TestSuite(minishell_test_extra_meta_test_alloc, .init = setup_alloc_mem);
+
+Test(minishell_test_extra_meta_test_alloc, build_token_lst_test_dolar_var)
+{
+	char	*input = "echo $USER";
+
+	res = token_lst_build(&token_lst, input);
+	types = fetch_tokens_type_list(token_lst);
+	values[0] = ft_strdup("echo");
+	values[1] = ft_strdup("$");
+	values[2] = ft_strdup("USER");
+	values[3] = NULL;
+	cr_assert_eq(res, 1);
+	cr_assert_eq(token_lst->next->type, DOLAR);
+	test_lst(token_lst, values, types);
+}
+ 
+Test(minishell_test_extra_meta_test_alloc, build_token_lst_dolar_char)
+{
+	char	*input = "echo $ USER";
+
+	res = token_lst_build(&token_lst, input);
+	types = fetch_tokens_type_list(token_lst);
+	values[0] = ft_strdup("echo");
+	values[1] = ft_strdup("$");
+	values[2] = ft_strdup("USER");
+	values[3] = NULL;
+	cr_assert_eq(res, 1);
+	cr_assert_neq(token_lst->next->type, DOLAR);
+	test_lst(token_lst, values, types);
+}
+
+Test(minishell_test_extra_meta_test, build_token_lst_wildcard_only)
+{
+	char	*input = "*";
+
+	values = ft_split(input, ' ');
 	res = token_lst_build(&token_lst, input);
 	types = fetch_tokens_type_list(token_lst);
 	cr_assert_eq(res, 1);
-	i = 0;
-	while (token_lst)
-	{
-		test_token_lst(token_lst, values[i], types[i]);
-		token_lst = token_lst->next;
-		i++;
-	}
-	token_lst_clear(&token_lst);
+	cr_assert_eq(token_lst->type, WILDCARD_SOLO);
+	test_lst(token_lst, values, types);
 }
- 
+
+Test(minishell_test_extra_meta_test_alloc, build_token_lst_wildcard_default)
+{
+	char	*input = "list *c";
+
+	res = token_lst_build(&token_lst, input);
+	types = fetch_tokens_type_list(token_lst);
+	values[0] = ft_strdup("list");
+	values[1] = ft_strdup("*");
+	values[2] = ft_strdup("c");
+	values[3] = NULL;
+	cr_assert_eq(res, 1);
+	cr_assert_eq(token_lst->next->type, WILDCARD_JOIN);
+	test_lst(token_lst, values, types);
+}
+
+Test(minishell_test_extra_meta_test_alloc, build_token_lst_wildcard_solo)
+{
+	char	*input = "list * c";
+
+	res = token_lst_build(&token_lst, input);
+	types = fetch_tokens_type_list(token_lst);
+	values[0] = ft_strdup("list");
+	values[1] = ft_strdup("*");
+	values[2] = ft_strdup("c");
+	values[3] = NULL;
+	cr_assert_eq(res, 1);
+	cr_assert_eq(token_lst->next->type, WILDCARD_SOLO);
+	test_lst(token_lst, values, types);
+}
+
+Test(minishell_test_extra_meta_test_alloc, build_token_lst_wildcard_solo_end)
+{
+	char	*input = "list *";
+
+	res = token_lst_build(&token_lst, input);
+	types = fetch_tokens_type_list(token_lst);
+	values[0] = ft_strdup("list");
+	values[1] = ft_strdup("*");
+	values[2] = NULL;
+	cr_assert_eq(res, 1);
+	cr_assert_eq(token_lst->next->type, WILDCARD_SOLO);
+	test_lst(token_lst, values, types);
+}
