ba690d81477eacf2cc5400295e89d276ac148e93
hebatist <hebaja@yahoo.com.br>
2025-05-21 17:40:17 -0300

    feat: 🎸 Redirect
    Treating redirect syntax problems

--- include/minishell.h
+++ include/minishell.h
@@ -75,6 +75,8 @@ int				quote_mode(t_token **token_lst, char **value,
 int				regular_mode(t_token **token_lst, char **value, 
 					char *value_start, int i);
 int				define_type_builtin(char *value, t_token_type *type);
+int				check_redirect(t_token *token_lst);
+int				token_lst_iterate_check(t_token *token_lst, int (*func)(t_token *));
 void			conclude_parser(t_token *token_lst);
 void			token_clear(t_token *token);
 void			token_lst_clear(t_token **token_lst);
--- src/parser_analyse.c
+++ src/parser_analyse.c
@@ -12,15 +12,34 @@
 
 #include "../include/minishell.h"
 
+void	builtin_echo(t_token *token_lst)
+{
+	if (token_lst->next && ft_strcmp(token_lst->next->value,"-n") == 0)
+		ft_printf("%s", token_lst->next->next->value);
+	else if (token_lst->next)
+		ft_printf("%s\n", token_lst->next->value);
+	else
+		ft_printf("\n");
+}
 
 /* TODO think about proper error msg when token integrity fails */
 int	analyse_token_lst(t_token **token_lst)
 {
+	int	res;
+
 	var_expansion(token_lst);
 	quotes_var_expansion(token_lst);
 	quote_removal(*token_lst);
 	token_joining(token_lst);
 	conclude_parser(*token_lst);
-	print_tokens(*token_lst);
+	res = check_redirect(*token_lst);
+	if (res == -1)
+		ft_putstr_fd("Syntax error\n", 2);
+	else
+	{
+		print_tokens(*token_lst);
+		if ((*token_lst)->type == BUILTIN_ECHO)
+			builtin_echo(*token_lst);
+	}
 	return (1);
 }
