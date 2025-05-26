079a8eabce1fc5afe585f92d3a3ed6545c478168
hebatist <hebaja@yahoo.com.br>
2025-05-26 15:47:33 -0300

    feat: 🎸 Redirect
    Attempt redirecting out done

--- src/parser_analyse.c
+++ src/parser_analyse.c
@@ -14,6 +14,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wait.h>
 
 void	builtin_echo(t_token *token_lst)
 {
@@ -36,31 +37,56 @@ t_token	*token_lst_get_next(t_token *token_lst, t_token_type type)
 	return (NULL);
 }
 
-void	exec(t_token *token_lst)
+int	solve_fd(t_token *token_lst)
+{
+	int	fd;
+
+	while (token_lst)
+	{
+		if (token_lst->next && token_lst->type == REDIRECT_OUT)
+		{
+			fd = open(token_lst->next->value, O_CREAT | O_RDWR | O_TRUNC, 0664);
+			if (fd == -1)
+			{
+				ft_putstr_fd("Open error\n", 2);
+				return (0);
+			}
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		token_lst = token_lst->next;
+	}
+	return (1);
+}
+
+int	exec(t_token *token_lst, int res)
 {
 	int	pid;
-	char	*file_name;
+	int	status;
 
 	if (token_lst->type != BUILTIN_ECHO)
-		return ;
+		return (0);
 	pid = fork();
 	if (pid == 0)
 	{
 		if (token_lst->type == BUILTIN_ECHO)
 		{
-			file_name = token_lst_get_next(token_lst, REDIRECT_OUT)->value;
-			
-
-			int	fd;
-			fd = open(file_name, O_CREAT | O_RDWR | O_TRUNC, 0664);
-			if (fd == -1)
-				ft_putstr_fd("Open error\n", 2);
-			dup2(fd, STDOUT_FILENO);
+			if (res)
+			{
+				if (!solve_fd(token_lst))
+					exit(EXIT_FAILURE);
+			}
 			builtin_echo(token_lst);
-			close(fd);
 			exit(EXIT_SUCCESS);
 		}
 	}
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+	{
+		if (WEXITSTATUS(status) == 0)
+			return (1);
+	}
+	return (0);
 }
 
 /* TODO think about proper error msg when token integrity fails */
@@ -75,11 +101,9 @@ int	analyse_token_lst(t_token **token_lst)
 	conclude_parser(*token_lst);
 	res = check_redirect(*token_lst);
 
-
-	res = check_redirect(*token_lst);
 	if (res == -1)
 		ft_putstr_fd("Syntax error\n", 2);
 	else
-		exec(*token_lst);
+		exec(*token_lst, res);
 	return (1);
 }
