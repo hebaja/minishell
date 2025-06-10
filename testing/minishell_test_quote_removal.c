4dfc7d20ec517407b7be1f571f23533812580ef3
hebatist <hebaja@yahoo.com.br>
2025-06-10 00:14:37 -0300

    refactor: 💡 Pipe-exec
    Refactoring for running with struct t_ms

--- testing/minishell_test_quote_removal.c
+++ testing/minishell_test_quote_removal.c
@@ -41,11 +41,13 @@ Test(minishell_test_suite_quote_removal, test_double_quote_removal_1)
 Test(minishell_test_suite_quote_removal, test_double_quote_removal_2)
 {
 	char	*input = "hello\"$USER\"";
+	char	*str;
 
 	res = token_lst_build(ms, input);
 	types = fetch_tokens_type_list(ms->token_lst);
 	cr_assert_eq(res, 1);
 	cr_assert_str_eq(ms->token_lst->value, "hello\"$USER\"");
 	usual_flow(ms, input);
-	cr_assert_str_eq(ms->token_lst->value, "hellohebatist");
+	str = ft_strjoin("hello", get_var_value(ms->env_lst, "USER"));
+	cr_assert_str_eq(ms->token_lst->value, str);
 }
